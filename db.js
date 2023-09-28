1aac679d1ad765c975f61c275ad7e075f26ee686
awakenjourney <mayer.sombat@gmail.com>
2023-09-28 14:39:09 -0600

    backend setup

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
 const POOL = require("pg").Pool;
 
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USER = process.env.DB_USER || "churmagoon";
+const DB = process.env.DB || "capstone_pizzeria_db";
+const DB_PASS = process.env.DB_PASS || "";
+const DB_PORT = process.env.DB_PORT || 5432;
+
 const pool = new POOL({
-  user: "churmagoon",
-  host: "localhost",
-  database: "capstone_pizzeria_db",
-  password: "",
-  port: 5432,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB,
+  password: DB_PASS,
+  port: DB_PORT,
 });
 
-module.exports = pool;
+module.exports = pool; 
